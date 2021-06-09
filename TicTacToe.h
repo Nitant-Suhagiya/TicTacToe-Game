f9371d5d1d80046b217e168c0c5667aebbd40af7
Nitant Suhagiya <71947520+Nitant-Suhagiya@users.noreply.github.com>
2021-06-08 19:35:18 -0600

    Add files via upload

---
+++ TicTacToe.h
@@ -0,0 +1,9 @@
+#pragma once
+
+static char square[10] = { 'o','1','2','3','4','5','6','7','8','9' };
+
+
+int checkwin();			//TO CHECK THE WIN
+void board();			//TO CREAT A BOARD
+void Game();			//TO PROCESS THE GAME
+void ResetArray();		//TO RESET THE ARRAY
